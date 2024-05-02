7b786f4d5167c3c7d49aa7c627638c6760238f5e
SaulGoodman-git <mihailbojko144@gmail.com>
2024-05-02 21:57:14 +0300

    New features added to Player class

--- src/GameLogic/Player.java
+++ src/GameLogic/Player.java
@@ -1,11 +1,68 @@
 package GameLogic;
 
-enum Color{
-    RED,
-    GREEN,
-    BLUE,
-    YELLOW
+enum Color {
+    RED("#F33",1),
+    GREEN("#3F3",2),
+    BLUE("#33F",3),
+    YELLOW("#EE4",3);
+
+    private String colorCode;
+    private int playerNumber;
+
+    private Color(String colorCode, int playerNumber) {
+        this.colorCode = colorCode;
+        this.playerNumber = playerNumber;
+    }
+
+    public String getColorCode(){
+        return colorCode;
+    }
 }
 
 public class Player {
+    private String name;
+    private Color color;
+    private boolean continueGame;
+    private int moneyAmount;
+    private Cell coordinates;
+
+    public Player(String name, boolean continueGame, int moneyAmount, Cell coordinates, Color color){
+        this.name = name;
+        this.continueGame = continueGame;
+        this.moneyAmount = moneyAmount;
+        this.coordinates = coordinates;
+        this.color = color;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public boolean getGameStatus(){
+        return continueGame;
+    }
+
+    public int getMoneyAmount(){
+        return moneyAmount;
+    }
+
+    public Cell getCoordinates(){
+        return coordinates;
+    }
+
+    public Color getColor(){
+        return color;
+    }
+
+    public void endGame(){
+        this.continueGame = false;
+    }
+
+    public void changeMoneyAmount(int moneyAmount){
+        this.moneyAmount = moneyAmount;
+    }
+
+    public void changeCoordinates(Cell coordinates){
+        this.coordinates = coordinates;
+    }
 }
